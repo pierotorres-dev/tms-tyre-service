657f95f27b564f4110ec87893a0367216a1cfaa4
pierotorres-dev <torresartica1701@gmail.com>
2025-07-29 23:13:41 -0500

    first commit

---
+++ src/main/java/com/dliriotech/tms/tyreservice/TmsTyreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dliriotech.tms.tyreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsTyreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsTyreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dliriotech/tms/tyreservice/TmsTyreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dliriotech.tms.tyreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsTyreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
